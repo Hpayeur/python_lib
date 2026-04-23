d4257f4ad80466c8ec955ae20fc21c66bf474f00
Hpayeur <hailpaye017@gmail.com>
2026-04-22 20:18:30 -0600

    madlibs

--- app.py
+++ app.py
@@ -1,2 +1,101 @@
+# Variables
+age = 50
+# price = 19.95
+# first_name = "john"
+# is_online = False
+print(age)
 
-print ("Hello World")
+# Receiving Input
+name = input("Enter Your Name")
+print("Hello " + name)
+
+# Type Conversion
+first = float(input("First"))
+second = input("Second")
+sum = float(first) + float(second)
+print("Sum: " + str(sum))
+
+# Strings
+# Examples: upper, lower, find, and replace
+course = 'Python For Beginners'
+print('Python' in course)
+
+# Arithmetic Operators
+# Examples: +, -, *, /, //, %, **, and +=
+x = (10 + 5) * 15
+print(x)
+
+# Comparison Operators
+# Examples: >, >=, <, <=, ==, and !=
+x = 3 > 2
+print(x)
+
+# Logical Operators
+# Examples: And = Both, Or = One at Least, Not
+price = 5
+print (price > 10 and price < 30)
+print(price > 10 or price < 30)
+print (not price > 10)
+
+# If Statements
+temperature = 90
+if temperature > 50:
+    print("It's A Hot Day")
+    print("Please Drink Lot's of Water")
+elif temperature > 20: # (20, 30]
+    print("It's a nice day ")
+elif temperature < 20: # (10, 20]
+    print("It's a bit cold")
+else:
+    print (" It's cold")
+    print("Done")
+
+# Exercise
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
+
+# While Loops
+i = 1
+while i <= 10:
+    print (i * '*')
+    i = i + 1
+# Lists
+names = ["John", "Lio", "Mario", "Anna", "Bryan"]
+names[0] = "Jessie"
+print(names[0:3])
+print(names)
+
+# List Methods
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+numbers.insert(0, -1)
+print(numbers)
+numbers.remove (3)
+print(numbers)
+numbers.clear()
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
+
+# For Loops
+numbers = [6, 7, 8]
+for item in numbers:
+    print(item)
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
+# The Range() Function
+for number in range(5):
+    print(number)
+# Tuples
+# Examples: count, and index
+numbers = (1, 2, 3, 3)
+numbers.count(3)
---
+++ madlib.py
@@ -0,0 +1,17 @@
+number = input("Let's pick a Number")
+name = input("Let's pick a Name")
+verb_ing = input("Let's pick a Verb_ing")
+clothing = input("Choose some clothing")
+adjective = input("Choose an adjective")
+adjective_ing = input("Choose an adjective_ing")
+noun = input("Let's pick a noun")
+liquid = input("What liquid will you choose?")
+
+print(f"Magicians have been around for{number} centuries."
+      f" One famous magicain was named{name} the{verb_ing} trickster! He had{number} invisibility{clothing}, twelve "
+      f"{verb_ing} doves, and one{adjective}, talking lion. He roamed the earth, searching for an apprentice."
+      f" However, all he found was an{adjective_ing}, annoying,{adjective}, smelly, troll."
+      f" This was no ordinary troll, though! It was a magical one, who quickly turned the magical one,"
+      f" who turned the magician into a{noun}, and tossed him into{liquid} Volcano! Yikes!")
+
+
